fc1e67c6319913b9e9d28fea2693e88a9236d2e8
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-06 19:43:28 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_name VARCHAR(14)
+);
+
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  room_name VARCHAR(14)
+);
+
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  text_message VARCHAR(50),
+  user_id INT,
+  room_id INT,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id)
+);
 
 
 
