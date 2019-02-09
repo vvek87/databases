d0ab6ae906f0d6bb0084373449bb741b6d51e6a3
aarushis.s.90@gmail.com <aarushisharma@MBP13-ASHAR.local>
2019-02-09 12:04:58 -0800

    pass post test

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,23 +3,27 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) {
-      models.messages.get((err, rows) => {
+      console.log('req.body in messages get ------------------------------', req.body);
+      models.messages.get((err) => {
+        console.log('err in controller messages get --------'. err);
         if (err) {
           console.log('error ', err);
         } else {
-          res.setHeader('content-type', 'application/json');
-          res.send({'rows': rows});
+          res.setHeader('content-type', 'text/plain');
+          res.send();
         }
       })
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      console.log('hello------------------------------');
-      models.messages.post((err, rows) => {
+      // console.log('req.body in messages post ------------------------------', req.body);
+      models.messages.post(req.body, (err, req) => {
+        console.log('hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii');
+        // console.log('req.body within models.messages.post --------', req.body);
         if (err) {
           console.log('error ', err);
         } else {
           res.setHeader('content-type', 'application/json');
-          res.send({'rows': rows}); //req.body
+          res.send({'rows': req.body}); //send 200 request
         }
       })
     } // a function which handles posting a message to the database
@@ -27,8 +31,28 @@ module.exports = {
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      console.log('req.body in users get -------------------------', req.body);
+      models.users.get((err, rows) => {
+        if (err) {
+          console.log('error ', err);
+        } else {
+          res.setHeader('content-type', 'text/plain');
+          res.send();
+        }
+      })
+    },
+    post: function (req, res) {
+      // console.log('req.body in users post ------------------------------', req.body);
+      models.users.post(req.body, (err, rows) => {
+        if (err) {
+          console.log('error ', err);
+        } else {
+          res.setHeader('content-type', 'application/json');
+          res.send({'rows': req.body}); //req.body
+        }
+      })
+    }
   }
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,7 @@ module.exports = {
   messages: {
     get: function (callback) {
       // connectDB.connect()
+      console.log('callback in models messages get -------------', callback);
       var query = "SELECT users.user_name, messages.text_message, rooms.room_name FROM messages INNER JOIN users ON users.id = messages.user_ID INNER JOIN rooms ON messages.room_ID = rooms.id";
 
       db.dbConnection.query(query, function(err, rows, fields) {
@@ -18,9 +19,9 @@ module.exports = {
       // db.dbConnection.end();
     }, // a function which produces all the messages
     post: function (message, callback) {
-      console.log('test---');
-      console.log('message----------', message);
-      db.dbConnection.query(`INSERT INTO messages (text_message) VALUES (+ ${message})`, function (err, rows, fields) {
+      // console.log('message in messages post---------', message)
+      // console.log('callback in messages post---------', callback)
+      db.dbConnection.query('INSERT INTO messages (text_message) VALUES ('+JSON.stringify(message.message)+')', function (err, rows, fields) {
         if (err) {
           console.log('Error while performing Query.', err);
           callback(err, null);
@@ -34,8 +35,32 @@ module.exports = {
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      console.log('callback in models users get ----------------------', callback);
+
+      db.dbConnection.query('SELECT * FROM users', function(err, rows, fields) {
+        if (err) {
+          console.log('Error while performing Query.', err);
+          callback(err, null);
+        } else {
+          console.log('rows and fields from get', rows);
+          callback(null, rows);
+        }
+      });
+    },
+    post: function (user, callback) {
+      // console.log('user in models users post--------------', user);
+      // console.log('callback in models users post----------', callback);
+      db.dbConnection.query('INSERT INTO users (user_name) VALUES ('+JSON.stringify(user.username)+')', function (err, rows, fields) {
+        if (err) {
+          console.log('Error while performing Query.', err);
+          callback(err, null);
+        } else {
+          console.log('rows and fields from post ', rows);
+          callback(null, rows);
+        }
+      });
+    }
   }
 };
 
